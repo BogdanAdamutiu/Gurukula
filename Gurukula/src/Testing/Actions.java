8f4ac81da2d6f9346a5dcb4d18f69df311d5e0fe
Bogdan Adamutiu <bogdan.adamutiu@gmail.com>
2018-03-11 09:21:12 +0100

    Added methods NavigateToBranch, NavigateToStaff, NavigateToAccountInformation, NavigateToPasswordChange

--- Gurukula/src/Testing/Actions.java
+++ Gurukula/src/Testing/Actions.java
@@ -22,15 +22,15 @@ public class Actions {
 		Mozila.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		log.info("Web application lanuched");
 		if (Mozila.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/div[2]/h1")).isDisplayed()) {
-			Reporter.log("Application lauched successfully | ");
+			Reporter.log("Application lauched successfully");
 		}
 		else {
-			Reporter.log("Application didn't lauched successfully | ");
+			Reporter.log("Application didn't lauched successfully");
 		}			
 	}
 	
 	@Test (dependsOnMethods = {"OpenBrowser"})
-	@Parameters({ "User", "Password" , "RememberMe"})
+	@Parameters ({ "User", "Password" , "RememberMe"})
 	public void Login(String User, String Password, Boolean RememberMe) throws InterruptedException, IOException {
 		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[1]/a[2]/span[2]")).click();
 		Thread.sleep(1000);
@@ -63,12 +63,71 @@ public class Actions {
 		
 		//check that we are logged in
 		if (Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul")).getAttribute("childElementCount").equalsIgnoreCase("4")) {
-			Reporter.log("Sign in successful | ");
+			Reporter.log("Sign in successful");
 		}
 		else {
-			Reporter.log("Sign in didn't work | ");
+			Reporter.log("Sign in didn't work");
 		}
 		Thread.sleep(1500);
 	}
 	
+	@Test (dependsOnMethods = {"Login"})
+	public void NavigateToBranch() throws InterruptedException {
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[2]/a/span/b")).click();
+		log.info("Click action performed on Entities drop down menu");
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[2]/ul/li[1]/a/span[2]")).click();
+		Thread.sleep(1000);
+		log.info("Click action performed on Branch");
+		if (Mozila.findElement(By.xpath("/html/body/div[3]/div[1]/div/h2")).getAttribute("innerText").equalsIgnoreCase("Branches")) {
+			Reporter.log("Opened Branch page successfully");
+		}
+		else {
+			Reporter.log("Branch page did not open");
+		}
+	}
+	
+	@Test (dependsOnMethods = {"Login"})
+	public void NavigateToStaff() throws InterruptedException {
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[2]/a/span/span[2]")).click();
+		log.info("Click action performed on Entities drop down menu");
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[2]/ul/li[2]/a/span[2]")).click();											
+		Thread.sleep(1000);
+		log.info("Click action performed on Staff");
+		if (Mozila.findElement(By.xpath("/html/body/div[3]/div[1]/div/h2")).getAttribute("innerText").equalsIgnoreCase("Staffs")) {
+			Reporter.log("Opened Staff page successfully");
+		}
+		else {
+			Reporter.log("Staff page did not open");
+		}
+	}
+	
+	@Test (dependsOnMethods = {"Login"})
+	public void NavigateToAccountInformation() throws InterruptedException {
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[3]/a/span/span[2]")).click();
+		log.info("Click action performed on Account drop down menu");
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[3]/ul/li[1]/a/span[2]")).click();
+		Thread.sleep(500);
+		log.info("Click action performed on Settings");
+		if (Mozila.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/div/h2")).getAttribute("innerText").equalsIgnoreCase("User settings for [admin]")) {
+			Reporter.log("Opened Account Information page successfully");
+		}
+		else {
+			Reporter.log("Account Information page did not open");
+		}
+	}
+	
+	@Test (dependsOnMethods = {"Login"})
+	public void NavigateToPasswordChange() throws InterruptedException {
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[3]/a/span/span[2]")).click();
+		log.info("Click action performed on Account drop down menu");
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[3]/ul/li[2]/a/span[2]")).click();
+		Thread.sleep(500);
+		log.info("Click action performed on Password");
+		if (Mozila.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/div/h2")).getAttribute("innerText").equalsIgnoreCase("Password for [admin]")) {
+			Reporter.log("Opened Password page successfully");
+		}
+		else {
+			Reporter.log("Password page did not open");
+		}
+	}
 }
