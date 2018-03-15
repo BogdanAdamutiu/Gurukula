24c97d29a66e0312d3e12952d59804a00c50fae8
Bogdan Adamutiu <bogdan.adamutiu@gmail.com>
2018-03-15 15:30:02 +0100

    Added method TestLoginRemember.

--- Gurukula/src/Testing/Actions.java
+++ Gurukula/src/Testing/Actions.java
@@ -1,14 +1,15 @@
 package Testing;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.ITestContext;
-import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
@@ -72,8 +73,7 @@ public class Actions {
 	}	 
 	  
 	@Test (dependsOnMethods = {"OpenBrowser"} , dataProvider = "Authentication")
-	public void Login(String User, String Password, String RememberMe) throws InterruptedException, IOException {
-		
+	public void Login(String User, String Password, String RememberMe) throws InterruptedException, IOException {		
 		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[1]/a[2]/span[2]")).click();
 		Thread.sleep(1000);
 		log.info("Click on Home button");
@@ -115,6 +115,58 @@ public class Actions {
 			Reporter.log("Sign in didn't work");
 		}
 		Thread.sleep(1500);
+	}
+	
+	@Test
+	public void TestRememberLogin() throws InterruptedException, IOException {
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[1]/a[2]/span[2]")).click();
+		Thread.sleep(1000);
+		log.info("Click on Home button");
+		
+		//click on login
+		Mozila.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/div[2]/div/div[1]/a")).click();	
+		Thread.sleep(1000);
+		log.info("Click on login button");
+		
+		//enter user name and password
+		Mozila.findElement(By.xpath("//*[@id=\"username\"]")).clear();
+		log.info("Cleared the Username text box");
+		Mozila.findElement(By.xpath("//*[@id=\"password\"]")).clear();
+		log.info("Cleared the Password text box");
+		Mozila.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("admin");
+		log.info("Username entered in the Username text box");
+		Mozila.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("admin");
+		log.info("Password entered in the Password text box");
+		
+		Mozila.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/div/form/button")).click();
+		Thread.sleep(2000);
+		log.info("Click action performed on Authentificate button");										
+		Thread.sleep(1500);
+		
+		//get cookies
+		Set<Cookie> All = Mozila.manage().getCookies();
+		Mozila.close();
+		FirefoxDriver Mozila = new FirefoxDriver();
+		Mozila.manage().window().maximize();;
+		Mozila.navigate().to("http://192.168.178.227:8080/");
+		Mozila.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		log.info("Web application launched");
+		for (Cookie cookie : All) {
+			Mozila.manage().addCookie(cookie);
+		}
+		Mozila.navigate().refresh();
+		Thread.sleep(2000);
+		Assert.assertTrue(Mozila.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/div[2]/div/div")).isDisplayed(), "User is no longer logged in! Remember me doesn't work");
+		Reporter.log("User is still logged in. Remember me works");
+		
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[3]/a/span/span[2]")).click();
+		log.info("Click action performed on Account drop down menu");
+		
+		Mozila.findElement(By.xpath("/html/body/div[2]/nav/div/div[2]/ul/li[3]/ul/li[4]/a/span[2]")).click();
+		Thread.sleep(1500);
+		log.info("Click action performed on Log out");
+		
+		Mozila.close();
 	}
 	
 	@Test (dependsOnMethods = {"Login"})
